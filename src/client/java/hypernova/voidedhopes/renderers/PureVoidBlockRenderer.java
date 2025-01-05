fe74c0c14b8d8226803b2aa770cfd25891b19b33
TywrapStudios <tywrap.studio@gmail.com>
2025-01-05 19:51:23 +0100

    Why was there an if (true) statement :sob:
    Took 9 minutes

--- src/client/java/hypernova/voidedhopes/renderers/PureVoidBlockRenderer.java
+++ src/client/java/hypernova/voidedhopes/renderers/PureVoidBlockRenderer.java
@@ -39,12 +39,10 @@ public class PureVoidBlockRenderer implements BlockEntityRenderer<PureVoidBlockE
     }
 
     private void renderSide(PureVoidBlockEntity entity, Matrix4f model, VertexConsumer vertices, float x1, float x2, float y1, float y2, float z1, float z2, float z3, float z4, Direction side) {
-        if(true) {
-            vertices.vertex(model, x1, y1, z1).next();
-            vertices.vertex(model, x2, y1, z2).next();
-            vertices.vertex(model, x2, y2, z3).next();
-            vertices.vertex(model, x1, y2, z4).next();
-        }
+        vertices.vertex(model, x1, y1, z1).next();
+        vertices.vertex(model, x2, y1, z2).next();
+        vertices.vertex(model, x2, y2, z3).next();
+        vertices.vertex(model, x1, y2, z4).next();
     }
 
     protected float getTopYOffset() {
