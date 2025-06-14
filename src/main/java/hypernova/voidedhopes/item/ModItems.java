728f3042bc3414d5af1ae5a078b8db857e0de2e8
Wither <deonvader55@gmail.com>
2025-06-14 21:43:27 +0200

    Added Items

--- src/main/java/hypernova/voidedhopes/item/ModItems.java
+++ src/main/java/hypernova/voidedhopes/item/ModItems.java
@@ -7,6 +7,18 @@ import net.minecraft.registry.Registry;
 
 public class ModItems {
 
+
+
+
+    public static final Item SOUL_EXTRACTOR = registerItem("soul_extractor",
+            new Item(new FabricItemSettings().group(ModItemGroup.VOIDEDHOPES_TAB)));
+    public static final Item voided_shard = registerItem("voided_shard",
+            new Item(new FabricItemSettings().group(ModItemGroup.VOIDEDHOPES_TAB)));
+    public static final Item wayfinder_closed = registerItem("wayfinder_closed",
+            new Item(new FabricItemSettings().group(ModItemGroup.VOIDEDHOPES_TAB)));
+    public static final Item wayfinder_open = registerItem("wayfinder_open",
+            new Item(new FabricItemSettings().group(ModItemGroup.VOIDEDHOPES_TAB)));
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, VoidedHopes.id(name), item);
     }
