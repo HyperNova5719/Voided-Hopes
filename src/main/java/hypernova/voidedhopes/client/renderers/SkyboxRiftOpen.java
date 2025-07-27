68c09cbc81b1dcdfbb3b275d550c722098bb3a6f
HyperNova <reczek.thomas@gmail.com>
2025-07-27 13:23:38 +0100

    fixed animation

---
+++ src/main/java/hypernova/voidedhopes/client/renderers/SkyboxRiftOpen.java
@@ -0,0 +1,8 @@
+package hypernova.voidedhopes.client.renderers;
+
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexFormat;
+import net.minecraft.client.render.VertexFormats;
+import nishio.lazuli_lib.core.LazuliBufferBuilder;
+import nishio.lazuli_lib.core.LazuliRenderingRegistry;
+import nishio.lazuli_lib.core.Transform3D;
